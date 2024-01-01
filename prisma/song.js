c31e5406557ed6e3ec36336d33703e0262201e0e
ADVSE\kasparek_jan <kasparek_jan@vseholding.sk>
2024-01-01 21:15:17 +0100

    pridane nejake male songy

--- prisma/song.js
+++ prisma/song.js
@@ -6,6 +6,26 @@ const song = [
 		lyrics: "dajake prve slova",
 		chords: "G D7 G C",
 	},
+	{
+		name: "Na Kráľovej holi",
+		lyrics: "dajake druhe slova",
+		chords: "G D7 G C",
+	},
+	{
+		name: "V pondelok doma nebudem",
+		lyrics: "dajake tretie slova",
+		chords: "G D7 G C",
+	},
+	{
+		name: "Mamko moja komiňare idú",
+		lyrics: "dajake štvrte slova",
+		chords: "G D7 G C",
+	},
+	{
+		name: "Á ja taká čárna",
+		lyrics: "dajake piate slova",
+		chords: "G D7 G C",
+	},
 ];
 
 module.exports = {
